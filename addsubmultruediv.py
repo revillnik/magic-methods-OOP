b70ac6a3c1e0d77687ba8aa4c54b5f6343cfc1ea
revillnik <revillnik@gmail.com>
2024-01-03 21:55:13 +0300

    last version

--- addsubmultruediv.py
+++ addsubmultruediv.py
@@ -11,19 +11,19 @@ class Number:
             )
 
     def __add__(self, other):
-        if type(other) not in (int, float, Number):
-            raise TypeError(
-                "Можно производить операции только с действительными или вещественными числами, либо объектами класса Number"
-            )
-        s = other
-        if type(other) == Number:
-            s = other.value
+        s = self.chek_in_operation(other)
         return Number(self.value + s)
 
     def __radd__(self, other):
         return self + other
 
     def __iadd__(self, other):
+        s = self.chek_in_operation(other)
+        self.value = self.value + s
+        return self
+
+    @classmethod
+    def chek_in_operation(self, other):
         if type(other) not in (int, float, Number):
             raise TypeError(
                 "Можно производить операции только с действительными или вещественными числами, либо объектами класса Number"
@@ -31,10 +31,74 @@ class Number:
         s = other
         if type(other) == Number:
             s = other.value
-        self.value = self.value + s
+        return s
+
+    def __sub__(self, other):
+        s = self.chek_in_operation(other)
+        return Number(self.value - s)
+
+    def __rsub__(self, other):
+        s = self.chek_in_operation(other)
+        return Number(s - self.value)
+
+    def __isub__(self, other):
+        s = self.chek_in_operation(other)
+        self.value = self.value - s
+        return self
+
+    def __mul__(self, other):
+        s = self.chek_in_operation(other)
+        return Number(self.value * s)
+
+    def __rmul__(self, other):
+        return self * other
+
+    def __imul__(self, other):
+        s = self.chek_in_operation(other)
+        self.value = self.value * s
+        return self
+
+    def __truediv__(self, other):
+        s = self.chek_in_operation(other)
+        return Number(self.value / s)
+
+    def __rtruediv__(self, other):
+        s = self.chek_in_operation(other)
+        return Number(s / self.value)
+
+    def __itruediv__(self, other):
+        s = self.chek_in_operation(other)
+        self.value = self.value / s
+        return self
+
+    def __floordiv__(self, other):
+        s = self.chek_in_operation(other)
+        return Number(self.value // s)
+
+    def __rfloordiv__(self, other):
+        s = self.chek_in_operation(other)
+        return Number(s // self.value)
+
+    def __ifloordiv__(self, other):
+        s = self.chek_in_operation(other)
+        self.value = self.value // s
+        return self
+
+    def __mod__(self, other):
+        s = self.chek_in_operation(other)
+        return Number(self.value % s)
+
+    def __rmod__(self, other):
+        s = self.chek_in_operation(other)
+        return Number(s % self.value)
+
+    def __imod__(self, other):
+        s = self.chek_in_operation(other)
+        self.value = self.value % s
         return self
 
 
-a = Number(1)
-a += 100
+a = Number(3)
+b = Number(4)
+a %= 3
 print(a.__dict__)
